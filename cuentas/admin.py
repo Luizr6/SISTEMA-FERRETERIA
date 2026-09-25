12e804cc287a81f5c4d912937ea5004516bc150e
Luis Frias <friasluis146@gmail.com>
2026-09-24 20:30:38 -0500

    Configurar modelo de usuario personalizado, migraciones y rest framework

--- cuentas/admin.py
+++ cuentas/admin.py
@@ -2,11 +2,4 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
-@admin.register(Usuario)
-class UsuarioAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ('Roles del Sistema', {'fields': ('rol',)}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Roles del Sistema', {'fields': ('rol',)}),
-    )
+admin.site.register(Usuario, UserAdmin)
